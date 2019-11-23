638fb2d04738b8e79831107b3dfa5192f042cd32
Jason2866 <obermeier.johann@googlemail.com>
2019-11-23 16:08:52 +0100

    Add files via upload

---
+++ build_32.bat
@@ -0,0 +1,3 @@
+c:\Python27_32\Scripts\pyinstaller.exe  --log-level=DEBUG ^
+            --noconfirm ^
+            dc-build-win32.spec
---
+++ build_64.bat
@@ -0,0 +1,3 @@
+c:\Python27\Scripts\pyinstaller.exe  --log-level=DEBUG ^
+            --noconfirm ^
+            dc-build-win.spec
