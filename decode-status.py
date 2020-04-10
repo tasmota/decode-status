1c481cb303121e783717cc82fef6c2d476dcfc01
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2020-04-10 17:04:14 +0200

    SevenSegDisplay

--- decode-status.py
+++ decode-status.py
@@ -2,28 +2,36 @@
 
 """
   decode-status.py - decode status for Tasmota
+
   Copyright (C) 2020  Theo Arends
+
   This program is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
   the Free Software Foundation, either version 3 of the License, or
   (at your option) any later version.
+
   This program is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
   GNU General Public License for more details.
+
   You should have received a copy of the GNU General Public License
   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 Requirements:
    - Python
    - pip json pycurl
+
 Instructions:
     Execute command with option -d to retrieve status report from device or
     get a copy of the status message with http command http://sonoff/cm?cmnd=status%200
     and store it in file status.json
+
 Usage:
     ./decode-status.py -d <hostname or IP address> [-u username] [-p password]
         or
     ./decode-status.py -f <JSON status information file>
+
 Example:
     ./decode-status.py -d sonoff1
     ./decode-status.py -d sonoff1 -p 12345678
@@ -85,7 +93,7 @@ a_setoption = [[
     "IR Unknown threshold",
     "CSE7766 invalid power margin",
     "Ignore hold time (s)",
-    "(not used) Number of Tuya MCU relays",
+    "Gratuitous ARP repeat time",
     "Over temperature threshold (celsius)",
     "(not used) Tuya MCU max dimmer value",
     "(not used) Tuya MCU voltage Id",
@@ -136,7 +144,9 @@ a_setoption = [[
     "PWM Dimmer Turn red LED on when powered off",
     "PWM Dimmer Buttons control remote devices",
     "Distinct MQTT topics per device for Zigbee",
-    "","","","",
+    "Disable non-json MQTT response",
+    "Enable light fading at start/power on",
+    "","",
     "","","","",
     "","","","",
     "","","","",
@@ -191,7 +201,7 @@ a_features = [[
     "USE_AHT1x","USE_WEMOS_MOTOR_V1","USE_DEVICE_GROUPS","USE_PWM_DIMMER"
     ],[
     "USE_KEELOQ","USE_HRXL","USE_SONOFF_D1","USE_HDC1080",
-    "","","","",
+    "USE_IAQ","USE_DISPLAY_SEVENSEG","","",
     "","","","",
     "","","","",
     "","","","",
