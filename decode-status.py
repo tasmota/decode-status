f61d3e5e1cadece29594b962f7304435f843b312
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2020-05-07 18:17:41 +0200

    Update decode-status.py

--- decode-status.py
+++ decode-status.py
@@ -194,12 +194,12 @@ a_features = [[
     ],[
     "USE_KEELOQ","USE_HRXL","USE_SONOFF_D1","USE_HDC1080",
     "USE_IAQ","USE_DISPLAY_SEVENSEG","USE_AS3935","USE_PING",
+    "USE_WINDMETER","USE_OPENTHERM","USE_THERMOSTAT","",
     "","","","",
     "","","","",
     "","","","",
     "","","","",
-    "","","","",
-    "","","",""
+    "","","","USE_WEBCAM"
     ]]
 
 usage = "usage: decode-status {-d | -f} arg"
@@ -233,7 +233,7 @@ else:
         obj = json.load(fp)
 
 def StartDecode():
-    print ("\n*** decode-status.py v20200428 by Theo Arends and Jacek Ziolkowski ***")
+    print ("\n*** decode-status.py v20200507 by Theo Arends and Jacek Ziolkowski ***")
 
 #    print("Decoding\n{}".format(obj))
 
@@ -302,4 +302,3 @@ if __name__ == "__main__":
         StartDecode()
     except Exception as e:
         print("E: {}".format(e))
-	
