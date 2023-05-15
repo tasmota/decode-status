491e290ae0fc86e625b3d74eb4a3d7336d9bbd6a
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2023-05-15 10:57:12 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
 #rm -fr build dist
-VERSION=1.0
-NAME=decode-status
 
 pyinstaller --log-level=DEBUG \
             --noconfirm \
             ds-build-mac.spec
 
 #https://github.com/sindresorhus/create-dmg
-create-dmg dist/$NAME-$VERSION.app
-mv "$NAME-$VERSION 0.0.0.dmg" dist/$NAME-$VERSION.dmg
+create-dmg decode-status.dmg dist
+mv *.dmg dist
